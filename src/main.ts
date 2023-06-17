b66d1d9b09d0b0187b4ba1134ae5a11987d62b54
Willem Verbuyst <willemverbuyst@gmail.com>
2023-06-17 07:59:00 +0200

    feat: add textarea

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,9 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div class="container">
+    <h1>FHIR Resource Validator</h1>
+    <label for="resourceInput">Add resource:</label>
+    <textarea id="resourceInput" name="resource input" rows="40" cols="50">...</textarea> 
   </div>
-`
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+`;
