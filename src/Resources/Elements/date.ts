54e6eccafb77da354a1e001481f4df98796e7095
Willem Verbuyst <willemverbuyst@gmail.com>
2023-07-06 05:56:54 +0200

    feat: create schema for patient

---
+++ src/Resources/Elements/date.ts
@@ -0,0 +1,6 @@
+import { z } from "zod";
+
+const dateRegex =
+  /([0-9]([0-9]([0-9][1-9]|[1-9]0)|[1-9]00)|[1-9]000)(-(0[1-9]|1[0-2])(-(0[1-9]|[1-2][0-9]|3[0-1]))?)?/;
+
+export const date = z.string().regex(dateRegex);
--- src/Resources/Patient.ts
+++ src/Resources/Patient.ts
@@ -1,11 +1,111 @@
 import { z } from "zod";
+import { Address } from "./Elements/Address";
+import { Attachment } from "./Elements/Attachment";
+import { CodeableConcept } from "./Elements/CodeableConcept";
+import { ContactPoint } from "./Elements/ContactPoint";
 import { DomainResource } from "./Elements/DomainResource";
+import { HumanName } from "./Elements/HumanName";
+import { Period } from "./Elements/Period";
+import { Reference } from "./Elements/Reference";
+import { date } from "./Elements/date";
+import { dateTime } from "./Elements/dateTime";
 import { Resources } from "./resources";
 
 export const Patient = DomainResource.extend({
   resourceType: z.literal(Resources.Patient),
   identifier: z.array(z.unknown()).optional(),
   active: z.boolean(),
-}).strict();
+  name: z.array(HumanName).optional(),
+  telecom: z.array(ContactPoint).optional(),
+  gender: z.enum(["male", "female", "other", "unknown"]).optional(),
+  birthDate: date.optional(),
+  deceasedBoolean: z.boolean().optional(),
+  deceasedDateTime: dateTime.optional(),
+  address: z.array(Address).optional(),
+  maritalStatus: CodeableConcept.optional(),
+  multipleBirthBoolean: z.boolean().optional(),
+  multipleBirthInteger: z.number().int().optional(),
+  photo: Attachment.optional(),
+  contact: z
+    .array(
+      z.object({
+        relationship: z.array(CodeableConcept).optional(),
+        name: HumanName.optional(),
+        telemcom: z.array(ContactPoint).optional(),
+        address: Address.optional(),
+        gender: z.enum(["male", "female", "other", "unknown"]).optional(),
+        organization: Reference(Resources.Organization).optional(),
+        period: Period.optional(),
+      })
+    )
+    .optional(),
+  communication: z
+    .array(
+      z.object({
+        language: CodeableConcept,
+        preferred: z.boolean().optional(),
+      })
+    )
+    .optional(),
+  generalPractitioner: Reference(Resources.PractitionerRole).optional(),
+  managingOrganization: Reference(Resources.Organization).optional(),
+  link: z
+    .array(
+      z.object({
+        other: Reference(Resources.Patient),
+        type: z.enum(["replaced-by", "replaces", "refer", "seealso"]),
+      })
+    )
+    .optional(),
+})
+  .strict()
+  .and(
+    z.union(
+      [
+        z.object({
+          deceasedBoolean: z.boolean(),
+          deceasedDateTime: z.undefined(),
+        }),
+        z.object({
+          deceasedBoolean: z.undefined(),
+          deceasedDateTime: dateTime,
+        }),
+        z.object({
+          deceasedBoolean: z.undefined(),
+          deceasedDateTime: z.undefined(),
+        }),
+      ],
+      {
+        errorMap: () => ({
+          message:
+            "Can't have both deceasedBoolean and deceasedDateTime at the same time",
+        }),
+      }
+    )
+  )
+  .and(
+    z.union(
+      [
+        z.object({
+          multipleBirthBoolean: z.boolean(),
+          multipleBirthInteger: z.undefined(),
+        }),
+        z.object({
+          multipleBirthBoolean: z.undefined(),
+          multipleBirthInteger: z.number().int(),
+        }),
+        z.object({
+          multipleBirthBoolean: z.undefined(),
+          multipleBirthInteger: z.undefined(),
+        }),
+      ],
+      {
+        errorMap: () => ({
+          message:
+            "Can't have both multipleBirthBoolean and multipleBirthInteger at the same time",
+        }),
+      }
+    )
+  );
 
 export type Patient = z.infer<typeof Patient>;
